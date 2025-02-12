02cce52c4fdc8ed3465d8b6cd461771dc1ad352b
httaX996 <damindakule@gmail.com>
2025-02-12 12:46:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'session id', //add your session id
-MONGODB: process.env.MONGODB || "mongodb url", //add your mongodb url
+SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=KQ0mXYSL#yi5BgCKITRoLTGBHzcSMPXUoIBcQmTZQ01K07ICjZVk', //add your session id
+MONGODB: process.env.MONGODB || "mongodb+srv://chethmina:LYKDcLfEgz4SjyMn@cluster0.7hf72.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
 };
